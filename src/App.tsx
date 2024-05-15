fff68ef3bca51bed2957793fdc0b95ca4666455f
Karm0s <18452314+Karm0s@users.noreply.github.com>
2024-05-14 22:26:07 -0400

    Add tailwindcss and NextUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Button } from '@nextui-org/react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleRunClick = (event) => {
+  }
+
 
   return (
     <>
-      <p>hello from extension</p>
+      <main className="text-foreground bg-background min-w-[500px] min-h-[500px] flex flex-col items-center justify-start">
+        <h1 className="text-3xl mt-4 tracking-tight font-bold">unsubby</h1>
+        <Button color="primary" className="font-bold mt-10" onClick={handleRunClick}>run</Button>
+      </main>
     </>
   )
 }
